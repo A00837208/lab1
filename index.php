0f8344ee33a80b94d200afb449a2e4d984e3d453
Andrew Walsh <andrew.walsh20@gmail.com>
2016-01-06 13:16:29 -0800

    Try it 6

--- index.php
+++ index.php
@@ -17,39 +17,33 @@ if (winner('x', $squares)) {
 }
 
 function winner($token,$position) {
-    $won = false;
-    if (($position[0] == $token) &&
-        ($position[1] == $token) &&
-        ($position[2] == $token)) {
-        $won = true;
-    } else if (($position[3] == $token) &&
-        ($position[4] == $token) &&
-        ($position[5] == $token)) {
-        $won = true;
-    } else if (($position[6] == $token) &&
-        ($position[7] == $token) &&
-        ($position[8] == $token)) {
-        $won = true;
-    } else if (($position[0] == $token) &&
-        ($position[3] == $token) &&
-        ($position[6] == $token)) {
-        $won = true;
-    } else if (($position[1] == $token) &&
-        ($position[4] == $token) &&
-        ($position[7] == $token)) {
-        $won = true;
-    } else if (($position[2] == $token) &&
-        ($position[5] == $token) &&
-        ($position[8] == $token)) {
-        $won = true;
-    } else if (($position[0] == $token) &&
-        ($position[4] == $token) &&
-        ($position[8] == $token)) {
-        $won = true;
-    } else if (($position[2] == $token) &&
-        ($position[4] == $token) &&
-        ($position[6] == $token)) {
-        $won = true;
+
+    $result = false;
+
+    for ($row = 0; $row < 3; $row++) {
+        if (($position[3 * $row] == $token) && ($position[3 * $row + 1]
+                == $token) && ($position[3 * $row + 2] == $token)
+        ) {
+            $result = true;
+        }
     }
-    return $won;
+
+    for ($col = 0; $col < 3; $col++) {
+        if (($position[$col] == $token) && ($position[$col+3]
+            == $token) && ($position[$col+6] == $token)) {
+            $result = true;
+        }
+    }
+
+    if (($position[0] == $token) &&
+        ($position[4] == $token) &&
+        ($position[8] == $token)) {
+            $result = true;
+    }
+    if (($position[2] == $token) &&
+        ($position[4] == $token) &&
+        ($position[6] == $token)) {
+            $result = true;
+    }
+        return $result;
 }
